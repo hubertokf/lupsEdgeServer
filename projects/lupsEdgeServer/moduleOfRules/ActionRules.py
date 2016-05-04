90657b4f65bc86a1f4ca7ccff8a90a59f14a7ca8
tainann <trcarvalho@inf.ufpel.edu.br>
2016-05-04 16:45:44 -0300

    verfificações json

--- projects/lupsEdgeServer/moduleOfRules/ActionRules.py
+++ projects/lupsEdgeServer/moduleOfRules/ActionRules.py
@@ -1,6 +1,6 @@
 from business_rules.actions import BaseActions, rule_action
 from business_rules.fields import FIELD_NUMERIC, FIELD_TEXT
-
+import simplejson as json
 class ActionRules(BaseActions):
 
     def __init__ (self, parameters):
@@ -14,3 +14,12 @@ class ActionRules(BaseActions):
     @rule_action(params={"test":FIELD_NUMERIC })
     def string_action(self,test):
         print (self.parameters.event,self.parameters.id,self.parameters.value)
+
+    @rule_action(params = {"inf": FIELD_TEXT})
+    def test_post_Event(self, inf):
+        params_event = inf
+        params_id = 18
+        decoJson = {'event' : params_event,
+                    'id' : params_id}
+        coJson = json.dumps(decoJson)
+        print(coJson)
