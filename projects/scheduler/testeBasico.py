b864aa6a79efb5a1ee4f35ccd3ce81b501d0f575
Leonardojoao01 <leonardojoao01@hotmail.com>
2016-05-12 15:10:12 -0300

    Teste como novo scheduler

--- projects/scheduler/testeBasico.py
+++ projects/scheduler/testeBasico.py
@@ -1,41 +1,64 @@
 from datetime import datetime
 import time
 import os
+import sqlite3
+import threading
 
 from apscheduler.schedulers.background import BackgroundScheduler
 
 
-def tick(a,b):
-	print(a,b)
+def tick(a):
+	print(a)
 	#print(b)
     #print('Tick! The time is: %s' % datetime.now())
 def te():
-    print("foi")
+	print("foi")
+
+def loop():
+	while True:
+	 	time.sleep(2)
+
 
 if __name__ == '__main__':
-    scheduler = BackgroundScheduler()
-  	
-    a = "bosta"
-    b = "jaja"
-    c = "nenhuma"
+	scheduler = BackgroundScheduler()
 
-    scheduler.add_job(tick, 'interval', seconds = 10,id = "ab", args=[a,b])
-    scheduler.add_job(tick, 'interval', seconds = 5,id = "bc", args=[c,b])
-    scheduler.start()
-    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+	conn = sqlite3.connect('dados_sensores.db')
+	cursor = conn.cursor()
 
-    a = 5
+	cursor.execute("""
+	SELECT * FROM dados_sensores;
+	""")
 
-    try:
+	for linha in cursor.fetchall():
+	#print(linha[2])
+		rslt = int(linha[2])
+		sensor = linha[1]
+		scheduler.add_job(tick, 'interval', seconds = rslt, id = sensor, args=[sensor])
+
+	conn.close()
+
+    #scheduler.add_job(tick, 'interval', seconds = 10,id = "ab", args=[a,b])
+	scheduler.start()
+	#https://docs.python.org/3.4/library/threading.html
+	#http://www.tutorialspoint.com/python/python_multithreading.htm
+	#thread = loop()
+	thread = threading.Thread(target=loop)
+
+	thread.start();
+	thread.join();
+
+#	a = 5
+
+#	try:
         # This is here to simulate application activity (which keeps the main thread alive).
-        a = 0
-        while True:
-            time.sleep(2)
-            if(a == 10):
-                scheduler.remove_job("bc")
-                scheduler.add_job(te, 'interval',seconds = a, id ="pica")
-            a = a + 1
-    except (KeyboardInterrupt, SystemExit):
-        # Not strictly necessary if daemonic mode is enabled but should be done if possible
-        scheduler.remove_job("bc")
+#		a = 0
+#		while True:
+#			time.sleep(2)
+#			if(a == 10):
+#				scheduler.remove_job("bc")
+#				scheduler.add_job(te, 'interval',seconds = a, id ="pica")
+#				a = a + 1
+#	except (KeyboardInterrupt, SystemExit):
+		# Not strictly necessary if daemonic mode is enabled but should be done if possible
+#		scheduler.remove_job("bc")
         #scheduler.shutdown()
