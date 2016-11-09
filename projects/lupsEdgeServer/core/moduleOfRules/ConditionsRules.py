9826c818e7c3006de7b89d8ad919c079facd5607
tainann <trcarvalho@inf.ufpel.edu.br>
2016-11-09 13:35:58 -0200

    fix most bugs

--- projects/lupsEdgeServer/core/moduleOfRules/ConditionsRules.py
+++ projects/lupsEdgeServer/core/moduleOfRules/ConditionsRules.py
@@ -29,10 +29,12 @@ class ConditionsRules(BaseVariables):
         data_condition          = json.loads(params)
         uuid['uuID']            = data_condition['sensor']
         info_gateway_and_sensor = gateways.coleting_value_of_sensor(uuid)
-        colletcDate             = datetime.datetime.now()
+        collectDate             = datetime.datetime.now()
+        format_colletcDate        = collectDate.strftime("%Y-%m-%d %H:%M:%S")
         value                   = info_gateway_and_sensor
-        self.parameters.create_obj_and_set_value(uuid['uuID'],value,colletcDate)
+        self.parameters.create_obj_and_set_value(uuid['uuID'],value,format_colletcDate)
         return value
+        
     @numeric_rule_variable
     def diff_values_sensor(self,params):
 
