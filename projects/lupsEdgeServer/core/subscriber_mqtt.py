a724e0711a9ccadf5771fb64aa708aaee4d68db0
Leonardojoao01 <ldrsjoao@inf.ufpel.edu.br>
2017-07-06 13:11:33 -0300

    Funcionando o MOSQUITTO com as regras

--- projects/lupsEdgeServer/core/subscriber_mqtt.py
+++ projects/lupsEdgeServer/core/subscriber_mqtt.py
@@ -5,11 +5,27 @@ A small example subscriber
 """
 from threading import Thread
 import paho.mqtt.client as paho
+import json
 
 class Subscriber(object):
     """docstring for subscriber."""
+
+
     def __init__(self, parent):
-        #threading.Thread.__init__(self)
+        def on_connect(client, userdata, flags, rc):
+            print(rc)
+
+        def on_message(mosq, obj, msg):
+        #print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+            #print(msg.payload)
+            self.new_json_result(msg.payload)
+            self.core
+            pass
+
+        def on_publish(mosq, obj, mid):
+            #print("mid: "+str(mid))
+            pass
+
         self.core = parent
 
         self.client = paho.Client()
@@ -24,36 +40,54 @@ class Subscriber(object):
         client_loop(self.client).start()
 
         # Verifica os TOPICOS existentes nas regras
-        #param = {"uuid":parameters['id']}
-        #id_sensor       = self.core.API_access("get", "rules", model_id=None, data=None, param=param).json()
-        #param = {"sensor":id_sensor[0]['id']}
 
         rules           = self.core.API_access("get", "rules", model_id=None, data=None, param=None).json()
-        #topic = rule[0]['topico']
-        # for key, value in param.items():
+
         for topico in rules:
-            print("++++++++++++++++++++++++++++")
-            #print(topico['topico'])
             self.add_subscribe(topico['topico'])
-            print("++++++++++++++++++++++++++++")
 
 
     def add_subscribe(self, topico):
-        print("++++++++++++++++++++++++++++")
-        #print(topico)
-        #print("++++++++++++++++++++++++++++")
-        #juca = topico
+        self.core
         self.client.subscribe(topico, 0)
 
-def on_connect(client, userdata, flags, rc):
-    print(rc)
+    def new_json_result(self, dados_publisher):
 
-def on_message(mosq, obj, msg):
-    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))
+        # Converte de bytes para string e após para json
+        my_new_string_value = dados_publisher.decode("utf-8")
+        my_json = json.loads(my_new_string_value)
 
-def on_publish(mosq, obj, mid):
-        #print("mid: "+str(mid))
-    pass
+        # Criar o JSON no formato para enviar as regras
+        # {'gateway': 1, 'uuID': '123e4567-e89b-12d3-a456-426655440001', 'event': 'gathering', 'collect_to_rule': True}
+
+        param = {"uuid":my_json['uuID']}
+        id_sensor       = self.core.API_access("get", "sensors", model_id=None, data=None, param=param).json()
+        id_gateway           = {"id":id_sensor[0]['gateway']}
+
+        json_rules={}
+
+        json_rules['id_gateway'] = id_gateway['id']
+        json_rules['id_sensor'] = my_json['uuID']
+        json_rules['value'] = my_json['value_sensor']
+        json_rules['collectDate'] = my_json['date']
+
+        self.regra(json_rules)
+
+
+
+    def regra(self,json_result_gathering):   # Verifica argumentos e criar objeto p chamar regras
+        engine = EngineRule(self.core)
+
+        string_rule = '{{ "evento": "e", "id": "{0}","valor": {1}, "id_gateway": {2} }}'.format(json_result_gathering['id_sensor'],json_result_gathering['value'],json_result_gathering['id_gateway'],json_result_gathering['collectDate'])
+        #print(string_rule)
+        #print("chegou no gathering, vai chamar regra")
+        #engine.run_rules(string_rule)
+
+from core.moduleOfRules.EngineRuleEdge import EngineRule
+
+
+
+
 
 class client_loop (Thread):
     def __init__(self,client):
